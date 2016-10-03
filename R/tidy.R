9fa8c7b742f9ca8823de778246ee35e0fae8e7c7
joethorley <joe@poissonconsulting.ca>
2016-10-03 14:22:08 -0700

    renamed conf.level as conf_level

--- R/tidy.R
+++ R/tidy.R
@@ -6,11 +6,10 @@
 #'
 #' @param terms A string of the terms to tidy. Permitted values are 'all', 'fixed',
 #' 'random' and 'report'.
-#' @param conf.level A number specifying the confidence level. By default 0.95.
+#' @param conf_level A number specifying the confidence level. By default 0.95.
 #' @param ... Unused.
 #' @seealso \code{\link[broom]{tidy}}.
 #' @export
-tidy.tmb_analysis <- function(x, terms = "fixed", conf.level = 0.95, ...) {
-  check_number(conf.level, c(0.5, 0.99))
-  coef(x, term = terms, conf_level = conf.level)
+tidy.tmb_analysis <- function(x, terms = "fixed", conf_level = 0.95, ...) {
+  coef(x, term = terms, conf_level = conf_level)
 }
