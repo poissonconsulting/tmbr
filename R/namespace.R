40eaad20e7928a1ebd77f6bef6d8fcbcda261f62
Joe Thorley <joe@poissonconsulting.ca>
2016-10-11 10:06:05 -0700

    importFrom datacheckr

--- R/namespace.R
+++ R/namespace.R
@@ -1,4 +1,5 @@
-#' @import datacheckr mbr
+#' @import mbr
+#' @importFrom datacheckr check_cols check_data1 check_data2 check_flag check_number check_string check_vector check_unique
 #' @importFrom magrittr %<>% %>%
 #' @importFrom stats coef confint fitted logLik predict residuals
 NULL
