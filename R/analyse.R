3f61caab883d6667bbe822960e3b1d6a7b46ec77
joethorley <joe@poissonconsulting.ca>
2017-04-24 20:56:33 -0700

    stop suppressing warnings

--- R/analyse.R
+++ R/analyse.R
@@ -36,12 +36,12 @@ lmcmcr <- function(object) {
   derived <- coef(object, "derived")
   random <- coef(object, "random")
 
-  # Warning in bind_rows_(x, .id): Vectorizing 'term' elements may not preserve attributes
-  suppressWarnings(coef %<>% dplyr::bind_rows(derived) %>% dplyr::bind_rows(random) %>% dplyr::select_(~term, ~estimate))
-
-  coef %<>% dplyr::mutate_(parameter = ~str_replace(term, "^(\\w+)(.*)", "\\1"))
-
-  coef %<>% plyr::dlply(~parameter, lmcmcarray)
+  coef %<>%
+    dplyr::bind_rows(derived) %>%
+    dplyr::bind_rows(random) %>%
+    dplyr::select_(~term, ~estimate) %>%
+    dplyr::mutate_(parameter = ~str_replace(term, "^(\\w+)(.*)", "\\1")) %>%
+    plyr::dlply(~parameter, lmcmcarray)
 
   class(coef) <- "mcmcr"
 
