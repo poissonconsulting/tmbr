01a33eb4b7ca22a61b6ace226f75af71069ce043
joethorley <joe@poissonconsulting.ca>
2019-08-26 09:47:39 -0700

    scalar only

--- R/namespace.R
+++ R/namespace.R
@@ -1,4 +1,5 @@
 #' @import checkr mcmcr mbr rlang stringr term
+#' @importFrom chk chk_lgl chk_unused
 #' @importFrom broom glance
 #' @importFrom coda thin
 #' @importFrom lubridate dminutes is.duration
--- R/pars.R
+++ R/pars.R
@@ -1,24 +1,25 @@
-pars_arg2to1 <- function(param_type, x, scalar_only, ...) {
-  pars(x = x, param_type = param_type, scalar_only = scalar_only, ...)
+pars_arg2to1 <- function(param_type, x, scalar, ...) {
+  pars(x = x, param_type = param_type, scalar = scalar, ...)
 }
 
 #' @export
-pars.tmb_code <- function(x, param_type = "all", scalar_only = FALSE, ...) {
+pars.tmb_code <- function(x, param_type = "all", scalar = NA, ...) {
   check_vector(param_type, c("fixed", "random", "derived", "primary", "all"),
                length = 1)
-  check_flag(scalar_only)
+  chk_lgl(scalar)
+  chk_unused(...)
 
   if (param_type %in% c("fixed", "random"))
     error("parameters.tmb_code is not currently able to separate 'fixed' or 'random' parameter types - set param_type = 'primary' instead")
 
-  if (param_type == "derived" && scalar_only)
-    error("parameters.tmb is not currently able to identify scalar 'derived' parameter types - set scalar_only = FALSE instead")
+  if (param_type == "derived" && !is.na(scalar))
+    error("parameters.tmb is not currently able to identify scalar 'derived' parameter types - set scalar = NA instead")
 
   if (param_type == "all") {
     pars <- c("primary", "derived")
 
     pars %<>%
-      purrr::map(pars_arg2to1, x = x, scalar_only = scalar_only, ...) %>%
+      purrr::map(pars_arg2to1, x = x, scalar = scalar, ...) %>%
       unlist() %>%
       sort()
 
@@ -28,10 +29,12 @@ pars.tmb_code <- function(x, param_type = "all", scalar_only = FALSE, ...) {
   x %<>% template() %>% str_replace_all(" ", "")
 
   if (param_type == "primary") {
-    if (scalar_only) {
+    if(is.na(scalar)) {
+      x %<>% str_extract_all("\\s(PARAMETER(|_VECTOR|_MATRIX|_ARRAY))[(]\\w+[)]", simplify = TRUE)
+    } else if(scalar) {
       x %<>% str_extract_all("\\s(PARAMETER)[(]\\w+[)]", simplify = TRUE)
     } else
-      x %<>% str_extract_all("\\s(PARAMETER(|_VECTOR|_MATRIX|_ARRAY))[(]\\w+[)]", simplify = TRUE)
+      x %<>% str_extract_all("\\s(PARAMETER(_VECTOR|_MATRIX|_ARRAY))[(]\\w+[)]", simplify = TRUE)
   } else # ignore REPORT parameters as easily generate using predict
     x %<>% str_extract_all("\\s(ADREPORT)[(]\\w+[)]", simplify = TRUE)
 
