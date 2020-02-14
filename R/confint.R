2dfe2dc67ca43a0e9419b963ccbc1b063a30e489
joethorley <joe@poissonconsulting.ca>
2020-02-14 14:43:46 -0800

    allow terms for parm

--- R/confint.R
+++ R/confint.R
@@ -78,7 +78,6 @@ confint.tmb_ml_analysis <- function(object, parm = terms(object),
   if (beep) on.exit(beepr::beep())
   beep <- FALSE
 
-  check_vector(parm, "")
   check_vector(level, c(0.5, 0.99), length = 1)
   check_flag(parallel)
 
